bb6c9a5de324ad7c9ef3dbfe424c855d7bc3f780
Richard Max <richard_mbs@hotmail.com>
2020-05-27 19:08:27 -0300

    adição pagina de apresentação e correção de um bug

--- comerga/settings.py
+++ comerga/settings.py
@@ -160,7 +160,7 @@ MEDIA_ROOT = MEDIA_URL
 
 STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 SECURE_SSL_REDIRECT = True
